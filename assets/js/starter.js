21474c667bb5f1355013091e46f6e543657ce973
Muathye <muath.ye@gmail.com>
2020-08-30 01:35:53 +0300

    +nice scroll with primary layout

--- assets/js/starter.js
+++ assets/js/starter.js
@@ -1,22 +1,60 @@
-// Importing JavaScript
-//
-// You have two choices for including Bootstrap's JS files—the whole thing,
-// or just the bits that you need.
+/* Importing JavaScript*/
+/**/
+/* You have two choices for including Bootstrap's JS files—the whole thing,*/
+/* or just the bits that you need.*/
 
 
-// Option 1
-//
-// Import Bootstrap's bundle (all of Bootstrap's JS + Popper.js dependency)
+/* Option 1*/
+/**/
+/* Import Bootstrap's bundle (all of Bootstrap's JS + Popper.js dependency)*/
 
 import "../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
 
 
-// Option 2
-//
-// Import just what we need
+/* Option 2*/
+/**/
+/* Import just what we need */
 
-// If you're importing tooltips or popovers, be sure to include our Popper.js dependency
-// import "../../node_modules/popper.js/dist/popper.min.js";
+/* If you're importing tooltips or popovers, be sure to include our Popper.js dependency*/
+/* import "../../node_modules/popper.js/dist/popper.min.js";*/
 
 import "../../node_modules/bootstrap/js/dist/util.js";
 import "../../node_modules/bootstrap/js/dist/modal.js";
+
+
+/**
+ * Nice Scroll
+ */
+import "../../node_modules/jquery.nicescroll/dist/jquery.nicescroll.js"
+/* 1. Simple mode, it styles document scrollbar:*/
+$(function() {
+  $("body").niceScroll(
+    {
+      /* background: "#F2F3F5",*/
+      cursorborder: "0",
+      cursorwidth: "10px",
+      cursorcolor: "#ced0d4",
+      rtlmode: "auto",
+      smoothscroll: true,
+      cursoropacitymin: 0.5,
+      autohidemode: "false",
+    }
+  ).resize();
+  $(".nicescroll-box").niceScroll(
+    ".nicescroll-box-wrap",
+    {
+      cursorborder: "0",
+      cursorwidth: "10px",
+      cursorcolor: "#ced0d4",
+      rtlmode: "auto",
+      nativeparentscrolling: true,
+      smoothscroll: true,
+      cursoropacitymin: 0.5,
+      scrollbarid: "#container-row-right-sticky",
+      autohidemode: "false",
+    }
+  );
+});
+/**
+ * ./Nice Scroll
+ */
